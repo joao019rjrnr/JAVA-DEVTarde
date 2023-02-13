e36f59564a7fcf0c84d44e9cd6fe861889a38196
joao019rjrnr <joaopedro185856@gmail.com>
2023-02-13 15:13:46 -0300

    Primeira  parte da aula 13/02

---
+++ CalculadoraDeDescontos2/Main.java
@@ -0,0 +1,9 @@
+package CalculadoraDeDescontos2;
+
+public class Main {
+    public static void main(String[] args) {
+        CalcularDescontos cDescontos = new CalcularDescontos();
+        cDescontos.calcula20();
+        cDescontos.calcula30();
+    }
+}
